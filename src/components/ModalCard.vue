9b3c8329a29a5891802974c505167a4424aa3920
Luis Contreras <luiscontreras20022005@gmail.com>
2025-03-10 02:35:41 -0600

    Feat: Mostrar trailer en detalles.

--- src/components/ModalCard.vue
+++ src/components/ModalCard.vue
@@ -19,6 +19,15 @@
           />
         </q-card-section>
   
+        <!-- Video del tráiler si existe -->
+        <q-card-section v-if="trailerKey">
+          <q-video
+            class="rounded-borders shadow-2"
+            :src="'https://www.youtube.com/embed/' + trailerKey"
+            style="height: 300px;"
+          />
+        </q-card-section>
+  
         <q-card-section class="row justify-center" style="width: 100%;">
           <p class="text-justify text-body1 text-white q-mt-md">{{ selectedMovie.overview || "Sin descripción disponible" }}</p>
         </q-card-section>
@@ -31,7 +40,8 @@
   </template>
   
   <script setup>
-  import { computed } from "vue";
+  import { ref, watch, computed } from "vue";
+  import { api } from "src/boot/axios";
   
   const props = defineProps({
     selectedMovie: Object,
@@ -39,11 +49,27 @@
   });
   
   const emit = defineEmits(["update:showModal"]);
+  const trailerKey = ref(null);
+  const API_KEY = '5e0199a60e32582860753e9dbd8af0f9';
   
   // Proxy para permitir modificar el showModal desde el componente padre
   const showModalProxy = computed({
     get: () => props.showModal,
     set: (value) => emit("update:showModal", value),
+  });
+  
+  // Obtener tráiler cuando se abre la modal
+  watch(() => [props.selectedMovie, props.showModal], async ([movie, isOpen]) => {
+    if (isOpen && movie.id) {
+      try {
+        const mediaType = movie.media_type || "movie";
+        const res = await api.get(`https://api.themoviedb.org/3/${mediaType}/${movie.id}/videos?api_key=${API_KEY}`);
+        const trailers = res.data.results.filter(v => v.type === "Trailer" && v.site === "YouTube");
+        trailerKey.value = trailers.length ? trailers[0].key : null;
+      } catch (error) {
+        console.error("Error al obtener el tráiler:", error);
+      }
+    }
   });
   </script>
   
