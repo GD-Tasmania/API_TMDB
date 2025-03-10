38255ca7242283cb2e20b63f4ae19bc90a7745a3
Luis Contreras <luiscontreras20022005@gmail.com>
2025-03-10 01:17:12 -0600

    Feat: Creación de Card y consulta de Peliculas Populares.

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <q-item clickable @click="emit('click', movie)" class="col-md-2 col-sm-5 col-xs-9 q-ma-md">
+      <q-card class="my-card col bg-dark" style="height: 390px;" flat bordered>
+        <q-img :src="'https://image.tmdb.org/t/p/w200' + movie.poster_path" alt="Movie Poster" class="imageCard full-width" fit="contain" />
+        <q-card-section class="bg-primary" style="height: 38%;">
+          <p class="col text-body1 movie-description">{{ movie.title || movie.name }}</p>
+          <p>⭐ {{ movie.vote_average || "Sin calificaciones" }}</p>
+          <p class="text-body5">{{ movie.release_date || movie.first_air_date || "Sin fecha de lanzamiento" }}</p>
+        </q-card-section>
+      </q-card>
+    </q-item>
+</template>
+  
+<script setup>
+  defineProps({
+    movie: Object
+  });
+  const emit = defineEmits(["click"]);
+</script>
+  
+<style scoped lang="scss">
+  .imageCard {
+    height: 62%;
+  }
+  .my-card {
+    border: 2px solid $primary;
+    border-radius: 5px;
+  }
+  .movie-description {
+    max-height: 65px; /* Ajusta según el diseño */
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2; /* Máximo 3 líneas */
+    -webkit-box-orient: vertical;
+    }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <!-- Cards -->
+    <q-list class="row justify-evenly">
+      <ItemCard 
+        v-for="movie in movies" 
+        :key="movie.id" 
+        :movie="movie"
+        @click="showDetails(movie)"
+      />
+    </q-list>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref, onMounted } from "vue";
+import { api } from "src/boot/axios"; 
+import ItemCard from "src/components/ItemCard.vue";
+
+const movies = ref([]);
+
+// Función para obtener películas populares
+const topMovies = async () => {
+  try {
+    const response = await api.get("/movie/popular");
+    movies.value = response.data.results;
+  } catch (error) {
+    console.error("Error al obtener películas", error);
+  }
+};
+
+// Cargar películas populares al inicio
+onMounted(() => topMovies());
 </script>
